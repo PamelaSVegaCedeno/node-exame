e7b9b474f9eb854c578130db878f9f9d009196f0
PamelaSVegaCedeno <pvega@akurey.com>
2023-07-31 16:30:17 -0600

    change gitignore

---
+++ src/index.js
@@ -0,0 +1,7 @@
+console.log("Hola mundo");
+const isOdd = require('is-odd');
+const number1 = 5;
+const number2 = 8;
+
+console.log(number1 + "es impar ?"+ isOdd(number1));
+console.log(number2 + "es impar ?"+ isOdd(number2));
